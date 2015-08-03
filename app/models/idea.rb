59a4e1e7db89b855711c7d5bd57848f0ccb0985c
naruto45me <russell.powers45@gmail.com>
2015-08-03 18:53:50 +0000

    add ideas with marketer id

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,3 +1,7 @@
 class Idea < ActiveRecord::Base
-
+    belongs_to :marketer
+    validates :marketer_id, presence: true  
+    validates :name, presence: true, length: { minimum: 5, maximum: 100}
+    validates :summary, presence: true, length: { minimum: 10, maximum: 150}
+    validates :description, presence: true, length: { minimum: 20, maximum: 500 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150729184641) do
+ActiveRecord::Schema.define(version: 20150803172449) do
 
   create_table "ideas", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150729184641) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "marketer_id"
   end
 
   create_table "marketers", force: :cascade do |t|
