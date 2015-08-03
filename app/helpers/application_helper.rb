0b80d57fb13e241c9315bed8079aacefe5474677
naruto45me <russell.powers45@gmail.com>
2015-08-03 19:22:30 +0000

    adding immage gravater

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+    def gravatar_for(marketer, options = { size: 80 })
+        gravatar_id = Digest::MD5::hexdigest(marketer.email.downcase)
+        size = options[:size]
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+        image_tag(gravatar_url, alt: marketer.marketername, class: "gravatar")
 end
