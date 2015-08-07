cc1af1781caec2fbc02e0aaec07e13e8b6020e12
naruto45me <russell.powers45@gmail.com>
2015-08-07 05:22:40 +0000

    the bob

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,26 @@ Rails.application.routes.draw do
   
   get '/home', to: 'pages#home'
   
-  resources :ideas
+  resources :ideas do
+    member do
+      post 'like'
+    end
+      resources :reviews, only: [:new, :create, :show]
+  end
+  
+  resources :marketer, except: [:new, :destroy]
+  
+  get '/register', to: 'marketers#new'
+  
+  get '/login', to: "logins#new"
+  post '/login', to: "logins#create"
+  get '/logout', to: "logins#destroy"
+  
+  resources :styles, only: [:new, :create, :show]
+  resources :ingredients, only: [:new, :create, :show]
+  
+  
+
+  
+
 end
